bd7a1769be036f96681e07213c05a4f10507c946
melekwellpack <melek.benzarti@wellpack.fr>
2025-06-13 15:18:22 +0200

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Start the Flask app using Gunicorn
+# Replace 'main:app' if your app object is named differently or in another file
+exec gunicorn main:app --workers 4 --bind 0.0.0.0:8000
